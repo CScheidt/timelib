f1c57e0ecd5aca1f534fe1e2311d2eb5ac18ee9b
CScheidt <christian.scheidt@icloud.com>
2018-12-20 18:15:15 +0100

    Added function to check if given year ist leapyear

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /**
- *
+ * Christian war hier.
  *
  *
  **/
@@ -17,8 +17,51 @@ int day_of_the_year(int day, int month, int year)
     return 0;
 }
 
+int isLeapyear(int year){
+
+    int leapyearBool = 0;
+
+    if (year < 1582){
+        return -1;
+    }
+
+    if((year % 4) == 0){
+
+        if((year % 100) == 0){
+            if(year % 400){
+                leapyearBool = 1;
+            } else {
+                leapyearBool = 0;
+            }
+        } else {
+            leapyearBool = 1;
+        }
+    } else {
+        leapyearBool = 0;
+    }
+    return leapyearBool;
+}
+
 int main()
 {
     printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
+	
+	int eingabeJahr = 0;
+
+    printf("Geben Sie ein mögliches Schlatjahr ein!\n");
+    scanf("%i", &eingabeJahr);
+
+    switch(isLeapyear(eingabeJahr)){
+        case 1:
+            printf("Eingegebenes Jahr ist ein Schlatjahr");
+            break;
+        case 0:
+            printf("Eingegebenes Jahr ist kein Schlaltjahr");
+            break;
+        case -1:
+            printf("Eingegebenes Jahr liegt vor 1582!");
+            break;
+    }
+	
     return 0;
 }
