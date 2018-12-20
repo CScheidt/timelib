9c2543fc96a707f0a24b0e43e641d8bbb58a4cab
CScheidt <christian.scheidt@icloud.com>
2018-12-20 19:11:59 +0100

    Gorobes Konstrukt aller Funktion erstellt und angepasst

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /**
- * Christian war hier.
+ * Author: Christian Scheidt
  *
  *
  **/
@@ -17,6 +17,29 @@ int day_of_the_year(int day, int month, int year)
     return 0;
 }
 
+void input_date(){
+
+}
+
+/// Die Funktion bestimmt für einen gegebenen Monat eines gegebenes Jahres, wie viele Tage der Monat hat.
+/// der Wert des Monats muss zwischen 1 und 12 liegen. Schaltjahre werden berücksichtigt.
+/// @params month: int, year: int
+int get_days_for_month(int month, int year){
+
+    int daysInMonth[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    return 0;
+}
+
+int exists_date(int day, int month, int year){
+
+    return 0;
+}
+
+
+///  Die Funktion überprüft, ob ein eingegebenes Jahr nach den Regeln des gregorianischen
+///  Kalendar ein Schaltjahr ist. Bei Jahreszahlen vor dem 1952 wird ein Fehler zurückgegeben.
+///  @params year: int
 int isLeapyear(int year){
 
     int leapyearBool = 0;
@@ -45,7 +68,7 @@ int isLeapyear(int year){
 int main()
 {
     printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
-	
+
 	int eingabeJahr = 0;
 
     printf("Geben Sie ein mögliches Schlatjahr ein!\n");
@@ -62,6 +85,6 @@ int main()
             printf("Eingegebenes Jahr liegt vor 1582!");
             break;
     }
-	
+
     return 0;
 }
