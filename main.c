acc58862eb7cee32f976238b942bc78dd427c64f
cscheidt <christian.scheidt@icloud.com>
2019-01-15 10:00:29 +0100

    Changed structur of repository

--- main.c
+++ main.c
@@ -1,50 +1,85 @@
 /**
  * Author: Christian Scheidt
- *
- *
+ * Date: 07.01.2019
+ * Content: erechnet für ein gegebenes Datum des gregorianischen Kalenders bestehend aus Tag, Monat
+ * und Jahr die Nummer des Tages, gezählt von Jahresbeginn (1. Januar) an.
  **/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- *
- *
- **/
+///
+///
+///
 int day_of_the_year(int day, int month, int year)
 {
+    int countDays = 0;
 
-    return 0;
+    if(exists_date(day, month, year) == 1){
+
+        for(int i = 1; i < month; i++ ){
+            countDays += get_days_for_month(i, year);
+        }
+    }
+
+    countDays += day;
+
+    return countDays;
 }
 
 void input_date(){
 
 }
 
-/// Die Funktion bestimmt fÃ¼r einen gegebenen Monat eines gegebenes Jahres, wie viele Tage der Monat hat.
-/// der Wert des Monats muss zwischen 1 und 12 liegen. Schaltjahre werden berÃ¼cksichtigt.
+/// Die Funktion bestimmt für einen gegebenen Monat eines gegebenes Jahres, wie viele Tage der Monat hat.
+/// der Wert des Monats muss zwischen 1 und 12 liegen. Schaltjahre werden berücksichtigt.
 /// @params month: int, year: int
 int get_days_for_month(int month, int year){
 
-    int daysInMonth[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int daysInMonth[14] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 0};
 
-    return 0;
+    if(exists_date(01, month, year) == 1) {
+
+        if(isLeapyear(year) == 1){
+            daysInMonth[2] = 29;
+        }
+        return daysInMonth[month];
+    } else {
+        return -1;
+    }
 }
 
+///
+///
+///
 int exists_date(int day, int month, int year){
 
-    return 0;
+    if(day < 1 || day > 31){
+        return 0;
+    } else if(month < 1 || month > 12){
+        return 0;
+    } else if(year < 1582 || year > 2400){
+        return 0;
+    } else if(month == 2 && day > 28){
+        if(isLeapyear(year) == 1 && day == 29){
+            return 1;
+        }else {
+            return 0;
+        }
+    }
+
+    return 1;
 }
 
 
-///  Die Funktion Ã¼berprÃ¼ft, ob ein eingegebenes Jahr nach den Regeln des gregorianischen
-///  Kalendar ein Schaltjahr ist. Bei Jahreszahlen vor dem 1952 wird ein Fehler zurÃ¼ckgegeben.
+///  Die Funktion überprüft, ob ein eingegebenes Jahr nach den Regeln des gregorianischen
+///  Kalendar ein Schaltjahr ist. Bei Jahreszahlen vor dem 1952 wird ein Fehler zurückgegeben.
 ///  @params year: int
 int isLeapyear(int year){
 
     int leapyearBool = 0;
 
-    if (year < 1582){
+    if (year < 1582 || year > 2400){
         return -1;
     }
 
@@ -67,11 +102,11 @@ int isLeapyear(int year){
 
 int main()
 {
-    printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
+    printf("Tag des Jahres: %i\n", day_of_the_year(12, 01, 2018));
 
 	int eingabeJahr = 0;
 
-    printf("Geben Sie ein mÃ¶gliches Schlatjahr ein!\n");
+    printf("Geben Sie ein mögliches Schlatjahr ein!\n");
     scanf("%i", &eingabeJahr);
 
     switch(isLeapyear(eingabeJahr)){
